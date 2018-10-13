fb7e96a788bf27eaff537be25c9a7fba19be4267
Andrew Chen <yongjhih@gmail.com>
2018-10-13 17:39:00 +0800

    Init

---
+++ constraints.js
@@ -0,0 +1,153 @@
+/* @flow */
+
+import type {
+  Email,
+  UUID,
+  Phone,
+  unsigned_number,
+  TimeZone,
+  PostalCode,
+  Country,
+  File,
+  URL,
+  IP,
+  Socket,
+  JsonProp
+} from './types';
+import * as validators from 'flow-runtime-validators';
+import Moment from 'moment';
+import MomentTimeZone from 'moment-timezone';
+import fs from 'fs';
+import validator from 'validator';
+//import faker from 'faker'; // TODO
+// TODO more fancy error message
+
+Email.addConstraint(
+  validators.length({max: 250}),
+  it => {
+    if (!validator.isEmail(it)) {
+      return "must be valid Email";
+    }
+  }
+);
+//Email.prototype.faker = (it: string): Email => { faker.internet.email(); };
+UUID.addConstraint(it => {
+  if (!validator.isUUID(it, 4)) {
+    return "must be valid UUID v4";
+  }
+});
+//UUID.prototype.faker = (it: string): UUID => { faker.random.uuid(); };
+
+Phone.addConstraint(
+  it => {
+    if (!validator.isMobilePhone(it, "any")) {
+      return "must be valid phone number";
+    }
+  }
+);
+//Phone.prototype.faker = (it: string): Phone => { faker.phone.phoneNumber(); };
+
+unsigned_number.addConstraint(
+  validators.number({gt: -1})
+);
+
+TimeZone.addConstraint(
+  it => {
+    if (!!MomentTimeZone.tz.zone(it)) {
+      // ok
+    } else {
+      return "must be valid time zone";
+    }
+  }
+);
+
+PostalCode.addConstraint(
+  it => {
+    if (!validator.isPostalCode(it, "any")) {
+      return "must be valid postal code";
+    }
+  }
+);
+//PostalCode.prototype.faker = (it: string): PostalCode => { faker.address.zipCode(); };
+
+Country.addConstraint(
+  it => {
+    if (!validator.isISO31661Alpha2(it, "any") || !validator.isUppercase(it)) {
+      return "must be valid country code";
+    }
+  }
+);
+//Country.prototype.faker = (it: string): Country => { faker.address.countryCode(); };
+
+// TODO contains(country.states())
+// TODO contains(country.provinces())
+File.addConstraint(
+  it => {
+    if (!fs.existsSync(it)) {
+      return "not found";
+    }
+  }
+);
+
+URL.addConstraint(
+  it => {
+    if (!validator.isURL(it)) {
+      return "must be valid URL";
+    }
+  }
+);
+
+JsonProp.addConstraint(
+  it => {
+    if (/[^a-zA-Z_]/.test(it)) {
+      return "must be valid Json property name of /[a-zA-Z_]/";
+    }
+  }
+);
+
+export const isDomainNameSocketLike = (it: string): boolean => {
+  return (/^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}:[0-9]+$/.test(it));
+};
+
+export const isDomainNameSocket = (it: string): boolean => {
+  if (isDomainNameSocketLike(it)) {
+    return validator.isPort(it.split(":")[1]);
+  }
+  return false;
+};
+
+export const isDomainName = (it: string): boolean => {
+  return (/^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/.test(it));
+};
+
+export const isIpSocketLike = (it: string): boolean => {
+  return (/^[0-9]+(.[0-9]{1,3}){1,3}:[0-9]+$/.test(it));
+};
+
+export const isIpSocket = (it: string): boolean => {
+  if (isIpSocketLike(it)) {
+    const ipSocket = it.split(":");
+    return validator.isIP(ipSocket[0]) && validator.isPort(ipSocket[1]);
+  }
+  return false;
+};
+
+export const isSocket = (it: string): boolean => {
+  return isIpSocket(it) || isDomainNameSocket(it);
+};
+
+Socket.addConstraint(
+  it => {
+    if (isSocket(it)) {
+      return "must be valid socket";
+    }
+  }
+);
+
+IP.addConstraint(
+  it => {
+    if (!validator.isIP(it, 4)) {
+      return "must be valid IP";
+    }
+  }
+);
---
+++ src/types.js
@@ -0,0 +1,17 @@
+/* @flow */
+/* @flow-runtime ignore */
+
+export type Socket = string;
+export type IP = string;
+export type Email = string;
+export type UUID = string;
+export type Phone = string;
+export type unsigned_number = number;
+export type TimeZone = string;
+export type PostalCode = string;
+export type Country = string;
+export type State = string;
+export type File = string;
+export type Time = string;
+export type URL = string;
+export type Enabled = "on" | "off" | "true" | "false" | "yes" | "no";
